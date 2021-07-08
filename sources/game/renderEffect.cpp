f8a1e779ee9bcb4b9bfb3c2a57e2bdc4b8014474
Tomas Maly <malytomas@ucpu.cz>
2021-07-09 01:57:06 +0200

    textured particles

--- sources/game/renderEffect.cpp
+++ sources/game/renderEffect.cpp
@@ -20,8 +20,11 @@ namespace
 	void engineUpdate()
 	{
 		const uint64 time = engineControlTime();
+		Entity *cam = engineEntities()->get(1);
+		CAGE_COMPONENT_ENGINE(Transform, ct, cam);
 		entitiesVisitor(engineEntities(), [&](Entity *e, TransformComponent &tr, const RenderEffectComponent &re) {
 			tr.position += re.move;
+			tr.orientation = quat(tr.position - ct.position, ct.orientation * vec3(0, 1, 0));
 			if (time > re.timeToDie)
 				e->destroy();
 		}, true);
@@ -40,6 +43,19 @@ namespace
 			engineUpdateListener.bind<&engineUpdate>();
 		}
 	} callbacksInstance;
+
+	uint32 renderName(const EffectConfig &config)
+	{
+		switch (config.type)
+		{
+		case DamageTypeEnum::Physical: return HashString("mazetd/particles/sprite.obj;physical");
+		case DamageTypeEnum::Fire: return HashString("mazetd/particles/sprite.obj;fire");
+		case DamageTypeEnum::Water: return HashString("mazetd/particles/sprite.obj;water");
+		case DamageTypeEnum::Poison: return HashString("mazetd/particles/sprite.obj;poison");
+		case DamageTypeEnum::Mana: return HashString("mazetd/particles/sprite.obj;mana");
+		default: return HashString("cage/model/fake.obj");
+		}
+	}
 }
 
 void renderEffect(const EffectConfig &config)
@@ -48,6 +64,8 @@ void renderEffect(const EffectConfig &config)
 	const real dist = distance(config.pos1, config.pos2);
 	const vec3 dir = normalize(config.pos2 - config.pos1);
 	const uint32 n = max(uint32(dist.value), 1u);
+	Entity *cam = engineEntities()->get(1);
+	CAGE_COMPONENT_ENGINE(Transform, ct, cam);
 	for (uint32 i = 0; i < n; i++)
 	{
 		Entity *e = engineEntities()->createAnonymous();
@@ -57,9 +75,10 @@ void renderEffect(const EffectConfig &config)
 		re.move = dir * (step / (real)dur);
 		CAGE_COMPONENT_ENGINE(Transform, tr, e);
 		tr.position = config.pos1 + dir * (i + 0.2);
-		tr.scale = 0.05;
-		tr.orientation = randomDirectionQuat();
+		tr.orientation = quat(tr.position - ct.position, ct.orientation * vec3(0, 1, 0));
 		CAGE_COMPONENT_ENGINE(Render, r, e);
-		r.object = HashString("cage/model/fake.obj");
+		r.object = renderName(config);
+		CAGE_COMPONENT_ENGINE(TextureAnimation, ta, e);
+		ta.startTime = randomRange(0u, 1000000000u);
 	}
 }
